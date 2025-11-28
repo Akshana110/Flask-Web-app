7b7e7ea08f66900c7f5ac12ebf20d2a568258235
Akshana <akshana.prasad.ug21@nsut.ac.in>
2025-11-29 05:28:24 +0530

    feat(phase-2): implement authentication, SQLAlchemy models & DB setup

--- website/__init__.py
+++ website/__init__.py
@@ -1,8 +1,16 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+
+db = SQLAlchemy()
+
+DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'akakashshanan'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{DB_NAME}"
+    db.init_app(app)
 
     from .views import views
     from .auth import auth
@@ -10,5 +18,17 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/') # registering the urls
 
+    from . import model
+
+    create_database(app)
+
     return app
+
+
+def create_database(app):
+    if not path.exists('website/' + DB_NAME):
+        with app.app_context():
+            db.create_all()
+        print('Created database!')
+    
 
